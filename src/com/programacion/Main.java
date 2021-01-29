75ec57d2b2d92a96f2656b729481409f00e6620c
dam1b <dmarinoalonso@danielcastelao.org>
2021-01-29 09:04:25 +0100

    1er commit

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -3,6 +3,8 @@ package com.programacion;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	public void commit(){
+            System.out.println("olee");
+        }
     }
 }
